19d95b23d84018f531acb9ff82ddc94cfaf7cdf7
Red_thz <248220094+revsync09-dot@users.noreply.github.com>
2025-12-19 09:57:19 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.GEOLIVE_CONFIG = {
-  CLIENT_ID: '1450090260200558643',
-  REDIRECT_URI: 'https://geo-live-dashboard.onrender.com/dashboard.html',
-  API_BASE_URL: 'https://YOUR-FLY-URL',
-  SCOPE: 'identify guilds'
-};
